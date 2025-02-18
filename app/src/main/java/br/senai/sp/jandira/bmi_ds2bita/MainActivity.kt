1b814bf2dff5be2c94327c2fac62261ff4ee0bfc
Prof Celso <celso.furtado@sp.senai.br>
2025-02-18 17:17:03 -0300

    Tela Home concluída

---
+++ app/src/main/java/br/senai/sp/jandira/bmi_ds2bita/MainActivity.kt
@@ -0,0 +1,21 @@
+package br.senai.sp.jandira.bmi_ds2bita
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import br.senai.sp.jandira.bmi_ds2bita.screens.HomeScreen
+import br.senai.sp.jandira.bmi_ds2bita.ui.theme.BMIDS2BITATheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BMIDS2BITATheme {
+                HomeScreen()
+            }
+        }
+    }
+}
+
